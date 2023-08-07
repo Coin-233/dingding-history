1d689a2adb566cbb5e0b12f694002414a87e5f4f
Coin-233 <coin233mail@qq.com>
2023-08-08 07:52:55 +0800

    check

---
+++ assets/scripts/config.js
@@ -0,0 +1,11 @@
+export default {
+  items: [
+    "meme/喝多不了一点.jpg",
+    "meme/大~雪~花~.jpg",
+    "meme/苹果14.jpg",
+    "meme/没有事.jpg",
+    "meme/喝多不了一点2.jpg",
+    "meme/14盆麦克斯.jpg",
+    "meme/晃晃晃晃晃晃晃晃晃晃.jpg"
+  ]
+}
